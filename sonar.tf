f12a989605831a0ddf79b2bdff54ceacac197860
iykmba2020 <iykmba20@gmail.com>
2026-07-25 02:00:18 -0500

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,54 +1,52 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0ee23bfc74a881de5"
-      key_name = var.key_name
-      instance_type = "t2.micro"
-      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
-      tags= {
-        Name = "sonar_instance"
-      }
-    }
+  ami                    = "ami-0ee23bfc74a881de5"
+  key_name               = var.key_name
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
 
- resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
-      description = "security group for Sonar"
+  tags = {
+    Name = "sonar_instance"
+  }
+}
 
-      ingress {
-        from_port   = 9000
-        to_port     = 9000
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+resource "aws_security_group" "sonar-sg-2022" {
+  name        = "security_sonar_group_2022"
+  description = "Security group for SonarQube"
+  vpc_id      = aws_vpc.main.id
 
-     ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-     # outbound from Sonar server
-      egress {
-        from_port   = 0
-        to_port     = 65535
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-      tags= {
-        Name = "security_sonar"
-      }
-    }
+  # Outbound traffic from SonarQube server
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Create Elastic IP address for Sonar instance
+  tags = {
+    Name = "security_sonar"
+  }
+}
+
+# Create Elastic IP address for SonarQube instance
 resource "aws_eip" "mySonarInstance" {
   instance = aws_instance.mySonarInstance.id
   domain   = "vpc"
 
   tags = {
-    Name = "sonar_elastic_ip"
-  }
-}
-tags= {
     Name = "sonar_elastic_ip"
   }
 }
