7f2425098a0fa24b93616f1b5988111fdc040903
iykmba2020 <iykmba20@gmail.com>
2026-07-25 01:38:47 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
-    key = "main"
-    region = "us-east-1"
+    bucket         = "iykmba-terraform-state-094794930722"
+    key            = "main"
+    region         = "us-east-2"
     dynamodb_table = "my-dynamo-db-table-tf"
+    encrypt        = true
   }
 }
